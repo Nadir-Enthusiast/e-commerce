d7422e7da5039418cc5baa048f31727e5680670e
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-07 13:59:01 -0400

    app

---
+++ day1/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* header */}
+      <Header />
+    </div>
+  );
+}
+
+export default App;
