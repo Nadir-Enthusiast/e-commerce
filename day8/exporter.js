c26a268b90f6f4fd1c01b1c84f90adfebb4002e3
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-22 13:59:41 -0400

    Create exporter.js

---
+++ day8/exporter.js
@@ -0,0 +1,9 @@
+// export all components
+import Header from './Header';
+import Home from "./Home";
+import Checkout from "./Checkout";
+import Login from './Login';
+import Payment from "./Payment";
+import Profile from "./Profile";
+
+export {Header, Home, Checkout, Login, Payment, Profile}
