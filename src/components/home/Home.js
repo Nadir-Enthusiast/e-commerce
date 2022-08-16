51a58484bcab0118b095697ddf988025f3416385
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-08-16 13:45:46 -0400

    Change layout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,11 @@
 import React,{useState} from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import "./Home.css"
+import "./Home.scss"
 import Product from "../product/Product"
+import sm1 from "../../pics/smartphone.png"
+import lp1 from "../../pics/laptop.png"
+import ar1 from "../../pics/airpods.png"
+import ds1 from "../../pics/desktop.png"
 
 const data = [
   {
@@ -27,75 +31,82 @@ function Home() {
 
   return (
     <div className='home'>
-        <div className="home-container">    
-            <Carousel style={{height: '500px', width: '900px', margin: '0 auto 25px auto'}} activeIndex={index} onSelect={handleSelect}>
-               {data.map((slide, i) => {
-                return (
-                  <Carousel.Item>        
-                    <img
-                      className="d-block w-100"
-                      src={slide.image}
-                      alt="slider image"
-                    />
-                  </Carousel.Item>
-                )
-                })}
-            </Carousel>
-            
-            <div className="home-row">
-                <Product 
-                    id="100000"
-                    title="Smartphone 1 type"
-                    image="https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    price={1299.99}
-                    rating={5}
-                />
-                <Product 
-                    id="100001"
-                    title="Another Smartphone"
-                    image="https://images.pexels.com/photos/249324/pexels-photo-249324.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    price={999.99}
-                    rating={4}
-                />
-                {/* two products*/}
-            </div>
-
-            <div className="home-row">
-                <Product
-                    id="100002"
-                    title="Sneakers"
-                    image="https://images.pexels.com/photos/8454917/pexels-photo-8454917.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    price={24.99}
-                    rating={4}
-                />
-                <Product 
-                    id="100003"
-                    title="Jacket"
-                    image="https://images.pexels.com/photos/7012347/pexels-photo-7012347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    price={28.99}
-                    rating={5}
-                />
-                <Product 
-                    id="100004"
-                    title="Jeans"
-                    image="https://images.pexels.com/photos/2343661/pexels-photo-2343661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                    price={29.99}
-                    rating={5}
-                />
-                {/* 3 products*/}
-            </div>
-
-            <div className="home-row">
-                <Product 
-                    id="100005"
-                    title="Apple Monitor"
-                    image="https://st2.depositphotos.com/2332949/6188/i/450/depositphotos_61882901-stock-photo-mock-up-pc-screen-blue.jpg"
-                    price={899.99}
-                    rating={5}
-                />
-                {/* 1 product*/}
-            </div>
+      <div className="intro-container">
+        <div className="intro">
+          <div class="intro-item ii1"></div>
+          <div class="intro-item ii2"></div>
+          <div class="intro-item ii3">
+            <h2>Available in 140+ countries</h2>
+            <p>We do our best to provide affordable shipping in as many countries as possible. Order goods nearly anywhere from the planet.</p>
+          </div>
+          <div class="intro-item ii4">4</div>
+          <div class="intro-item ii5">
+            <div className="background"></div>
+            <h2>Scroll down to see all</h2>
+          </div>
+          <div class="intro-item ii6">6</div>
         </div>
+      </div>
+      <div className="trending-container">          
+        <div className="title">
+          <h1>Explore Top Trending products</h1>
+        </div>
+        <div className="home-row">
+          <Product 
+            id="100000"
+            title="Samsung A50"
+            description="Announced Feb 2019. Features 6.4″ display, Exynos 9610 chipset, 4000 mAh battery, 128 GB storage, 6 GB RAM."
+            image={sm1}
+            price={129.99}
+            rating={5}
+            wid="22vw"
+          />
+          <Product 
+            id="100001"
+            title="HP Chromebook"
+            description="Chrome OS. Intel Pentium Silver, UHD Graphics4 GB memory, diagonal HD touch display"
+            image={lp1}
+            price={269.99}
+            rating={4}
+            wid="22vw"
+          />
+          <Product 
+            id="100002"
+            title="Airpods"
+            description="Earbuds. They were first announced on September 7, 2016, alongside with iPhone 7"
+            image={ar1}
+            price={129.99}
+            rating={5}
+            wid="22vw"
+          />
+          <Product 
+            id="100002"
+            title="Dell XPS 9840"
+            description="All in one computer. Windows 11 Home, 32gb RAM, 1048gb SSD, Intel i7 12th gen. Free Delivery."
+            image={ds1}
+            price={1549.99}
+            rating={5}
+            wid="22vw"
+          />
+        </div>
+      </div>
+      <div className="info-container">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          {data.map((slide, i) => {
+            return (
+              <Carousel.Item>        
+                <img
+                  src={slide.image}
+                  alt="slider image"
+                />
+              </Carousel.Item>  
+            )
+          })}
+        </Carousel>
+        <div className="side-box">
+
+        </div>
+      </div>
     </div>
   )
 }
